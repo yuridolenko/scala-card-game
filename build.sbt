613ce350348cec3f952e024ff31666024f7e41b1
ydolenko <yurii.dolenko@tain.com>
2019-03-01 16:47:34 +0200

    DeckGeneratorTest added, needed dependencies added

--- build.sbt
+++ build.sbt
@@ -9,6 +9,7 @@ libraryDependencies ++= Seq(
   "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2",
   "com.typesafe.akka" %% "akka-actor" % "2.5.21",
 
-  "org.scalatest" %% "scalatest" % "3.2.0-SNAP10" % Test
+  "org.scalatest" %% "scalatest" % "3.2.0-SNAP10" % Test,
+  "org.scalacheck" %% "scalacheck" % "1.14.0" % Test
 )
 
--- src/test/scala/com/test/util/DeckGeneratorTest.scala
+++ src/test/scala/com/test/util/DeckGeneratorTest.scala
@@ -6,7 +6,12 @@ class DeckGeneratorTest extends FlatSpec with Matchers {
 
   "DeckGenerator" should "generate deck with given size" in {
     val deck = DeckGenerator.generate(10, 10)
-    deck.size should be (10)
+    deck should have size 10
+  }
+
+  "DeckGenerator" should "generate deck card weight not greater than given" in {
+    val deck = DeckGenerator.generate(10, 10)
+    deck.foreach(_.weight should be <= 10)
   }
 
 }
