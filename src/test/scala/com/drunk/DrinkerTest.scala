e7db1726b1dde4d78aa3b6086b8d8be25ce83e90
ydolenko <yurii.dolenko@tain.com>
2019-04-05 12:29:03 +0300

    test fixed

--- src/test/scala/com/drunk/DrinkerTest.scala
+++ src/test/scala/com/drunk/DrinkerTest.scala
@@ -1,21 +1,13 @@
 package com.drunk
 
-import com.drunk.Grade.{JUNIOR, EXPERT}
+import com.drunk.Grade.{JUNIOR, SENIOR, EXPERT}
 import com.drunk.State.{HAPPY, DEAD}
 import org.scalatest.{FlatSpec, Matchers}
 import com.drunk.drinks.{Beer, Vodka, Wine}
 
 class DrinkerTest extends FlatSpec with Matchers {
 
-  "Junior Drinker" should "drink and get happy" in {
-    val drinker = Drinker("Dima", JUNIOR)
-
-    drinker drink Vodka() +50
-
-    assert(drinker.state == HAPPY)
-  }
-
-  "Drinker" should "die" in {
+  "Junior Drinker" should "die" in {
     val drinker = Drinker("drinker", JUNIOR)
 
     drinker drink Vodka() +150
@@ -27,6 +19,14 @@ class DrinkerTest extends FlatSpec with Matchers {
     drinker drink Vodka() +50
 
     assert(drinker.state == DEAD)
+  }
+
+  "Senior Drinker" should "drink and get happy" in {
+    val drinker = Drinker("Dima", SENIOR)
+
+    drinker drink Beer() +500
+
+    assert(drinker.state == HAPPY)
   }
 
   "Expert Drinker" should "always be happy" in {
